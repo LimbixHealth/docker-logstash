0ba1fa00d86a8104a07ca21574e1fdc14427fd28
William Schurman <wschurman@gmail.com>
2017-06-15 17:58:22 -0700

    No stdout/stderr in logstash for aptible

--- bin/run-logstash.sh
+++ bin/run-logstash.sh
@@ -5,4 +5,4 @@ set -o nounset
 erb /logstash.config.erb > /logstash-${LOGSTASH_VERSION}/logstash.config
 
 cd /logstash-${LOGSTASH_VERSION}
-exec bin/logstash -f logstash.config "$@"
+exec bin/logstash -f logstash.config "$@" > /dev/null 2>&1
